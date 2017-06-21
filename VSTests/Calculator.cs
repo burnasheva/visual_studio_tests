051d366c43f3a0f14c0615af024e98bcf2bd8625
Nadia Burnasheva@ <nadezhda.burnasheva@gmail.com>
2017-06-21 17:13:07 +0200

    Initial commit

---
+++ VSTests/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace VSTests
+{
+    public static class Calculator
+    {
+        public static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ VSTests/CalculatorTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VSTests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void TestPlus()
+        {
+            Assert.AreEqual(Calculator.Plus(1, 5), 6, "Wrong result");
+        }
+    }
+}
